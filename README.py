d0e574b0ba554e8f247a33bc2750a85cae3a640d
misha <mladygin@usetech.ru>
2021-08-02 10:43:05 +0300

    improve description project

--- README.py
+++ README.py
@@ -1,1 +1,8 @@
 # Edu phyton_tests
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.get('https://mail.ru/')
+driver.find_element_by_xpath("//*[@id='mailbox']/div[1]/a").click()
+#webdriver.findElement(By.xpath("/html/body/main/div[1]/div[1]/div/div/div[1]/a[3]")).click()
+assert "mail" in driver.page_source
+driver.quit()
